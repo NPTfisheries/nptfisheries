e900e223210024b21780af9e8e29570a871f9469
Tyler Stright <tylers@nezperce.org>
2023-02-01 17:27:33 -0700

    modified a few settings to allow CSRF token authentication. Was able to successfully login from React UI. Wanted to commit and save this before I screwed it up.  Don't pull request this until we learn more. -TS

--- nptfisheries/settings.py
+++ nptfisheries/settings.py
@@ -66,9 +66,12 @@ INSTALLED_APPS = [
     'documents',
     'locations',
 
+    'corsheaders'
+
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -77,6 +80,13 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+# whitelist apparently unnecessary for my ReactJS requests (TS 1/31/23).
+# CORS_ORIGIN_WHITELIST = [
+#     'http://localhost:3000'
+# ]
+
+CSRF_TRUSTED_ORIGINS = ['http://localhost:8000/'] # needs trailing slash (TS 1/31/23)
 
 ROOT_URLCONF = 'nptfisheries.urls'
 
