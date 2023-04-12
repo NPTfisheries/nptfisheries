f34efd9f46362524cfdb10d109a184b47ac38476
Ryan <ryank@nezperce.org>
2023-04-12 13:47:13 -0600

    rearranged some navbar links and associated link with view

--- data/urls.py
+++ data/urls.py
@@ -6,8 +6,8 @@ from . import views
 app_name = 'data'
 
 urlpatterns = [
-    path('esu_status/', views.esu_status, name = 'esu_status'),
-    path('window_counts/', views.window_counts, name = 'window_counts'),
+    path('window_counts/', views.window_counts, name = 'spsm_inseason'),
+    path('esu_status/', views.esu_status, name = 'spsm_status'),
     path('weir_counts/', views.weir_counts, name = 'weir_counts'),
     path('harvest_counts/', views.harvest_counts, name='harvest_counts'),
 ]
