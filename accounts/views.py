1e0c5046ce64bb73b5fccbf935fa77dbcf020680
Ryan <ryank@nezperce.org>
2023-05-09 13:57:58 -0600

    cleaned up project listings on profile page and made publications a link

--- accounts/views.py
+++ accounts/views.py
@@ -1,8 +1,9 @@
 # accounts/views.py
 from django.shortcuts import render, get_object_or_404, redirect
+from django.conf import settings
 #from django.contrib.auth.decorators import user_passes_test
 #from django.views.generic import CreateView
-from .models import UserProfile
+from accounts.models import UserProfile
 from dfrm_admin.models import Project, Subproject, Task
 from documents.models import Document
 #from django.contrib.auth import get_user_model
@@ -17,17 +18,40 @@ def profile(request, pk):
     
     project = Project.objects.filter(project_leader = profile.employees)
     subproject = Subproject.objects.filter(supervisor = profile.employees)
-    supervise = Task.objects.filter(supervisor = profile.employees)
+    task = Task.objects.filter(supervisor = profile.employees)
     staff = Task.objects.filter(staff = profile.employees)
+
+    project_names = set()
+    for obj in project:
+        project_names.add(obj.name)
+    for obj in subproject:
+        project_names.add(obj.project.name)
+    for obj in task:
+        project_names.add(obj.subproject.project.name)
+    for obj in staff:
+        project_names.add(obj.subproject.project.name)
+
+    # projects = {}
+    # for obj in project:
+    #     projects[obj.project.id] = {'id': obj.id, 'name': obj.name}
+    # for obj in subproject:
+    #     projects[obj.project.id] = {'id': obj.project.id, 'name': obj.project.name}
+    # for obj in task:
+    #     projects[obj.project.id] = {'id': obj.project.id, 'name': obj.subproject.project.name}
+    # for obj in staff:
+    #     projects[obj.project.id] = {'id': obj.project.id, 'name': obj.subproject.project.name}
+
 
     author = Document.objects.filter(employee_authors = profile.employees)
     
     context = {'profile': profile,
+               'project_names':project_names,
                'project': project,
                'subproject': subproject,
-               'supervise': supervise,
+               'task': task,
                'staff': staff,
-               'author': author}
+               'author': author,
+               'MEDIA_URL': settings.MEDIA_URL}
     
     return render(request, 'profile/profile.html', context)
 
